97ebbf1422239f1455061d1edb145a289e0eb712
Damien Seguin <dmnsgn@gmail.com>
2023-02-01 15:29:20 +0000

    feat: add support for typed array geometries and update positions in place
    - add test
    
    BREAKING CHANGE: use ESM

---
+++ test/index.js
@@ -0,0 +1,73 @@
+import test from "node:test";
+import assert from "node:assert";
+
+import centerAndNormalize from "../index.js";
+
+test("should normalize an array and return that array", (t) => {
+  // prettier-ignore
+  const positions = new Float32Array([
+    1, 1, 1,
+    2, 2, 2,
+    3, 5, 3
+  ]);
+
+  // prettier-ignore
+  const normalizedPositions = new Float32Array([
+    -0.25, -0.5, -0.25,
+    0, -0.25, 0,
+    0.25, 0.5, 0.25,
+  ]);
+
+  assert.deepStrictEqual(centerAndNormalize(positions), normalizedPositions);
+  assert.equal(centerAndNormalize(positions), positions);
+});
+
+test("should normalize an array of positions and return that array", (t) => {
+  // prettier-ignore
+  const positions = [
+    [1, 1, 1],
+    [2, 2, 2],
+    [3, 5, 3]
+  ];
+
+  // prettier-ignore
+  const normalizedPositions = [
+    [-0.25, -0.5, -0.25],
+    [0, -0.25, 0],
+    [0.25, 0.5, 0.25],
+  ];
+
+  assert.deepStrictEqual(centerAndNormalize(positions), normalizedPositions);
+  assert.equal(centerAndNormalize(positions), positions);
+});
+
+test("should normalize positions at unit bounds to the unit bbox", (t) => {
+  // prettier-ignore
+  const positions = new Float32Array([
+    0.5, 0, 0,
+    -0.5, 0, 0,
+
+    0, 0.5, 0,
+    0, -0.5, 0,
+
+    0, 0, 0.5,
+    0, 0, -0.5,
+  ]);
+
+  assert.deepStrictEqual(centerAndNormalize(positions), positions);
+});
+
+test("should normalize positions uniformly", (t) => {
+  // prettier-ignore
+  const positions = new Float32Array([
+    1, 1, 1,
+    -1, -1, -1,
+  ]);
+  // prettier-ignore
+  const normalizedPositions = new Float32Array([
+    0.5, 0.5, 0.5,
+    -0.5, -0.5, -0.5,
+  ]);
+
+  assert.deepStrictEqual(centerAndNormalize(positions), normalizedPositions);
+});
